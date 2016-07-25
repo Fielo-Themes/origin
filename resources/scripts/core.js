5659057503c627d4dd838145015cc8add2140bec
Hugo Gómez Mac Gregor <hugogmg@gmail.com>
2016-07-25 16:09:57 -0300

    Add support for google fonts

--- resources/scripts/core.js
+++ resources/scripts/core.js
@@ -1,3 +1,12 @@
+WebFontConfig = {
+  google: {families: ['Open+Sans:300,400,700:latin']}
+};
 (function() {
-  'use strict';
+  var wf = document.createElement('script');
+  wf.src = (document.location.protocol === 'https:' ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
 })();
